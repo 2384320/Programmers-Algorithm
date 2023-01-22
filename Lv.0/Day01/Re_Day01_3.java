ae665afaee91dfc00bb57cfdf7d4a9078daa5970
6720 <71584474+2384320@users.noreply.github.com>
2023-01-23 00:38:44 +0900

    Update Re_Day01_3.java

--- Lv.0/Day01/Re_Day01_3.java
+++ Lv.0/Day01/Re_Day01_3.java
@@ -1,8 +1,6 @@
-/* 두 수의 곱
-* 정수 num1, num2가 매개변수 주어집니다.
-* num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요. */
 class Solution {
     public int solution(int num1, int num2) {
         return num1 * num2;
     }
 }
+// return으로 바로 값이 빠지도록 별도 변수 생성 없이 실행되도록 함.
