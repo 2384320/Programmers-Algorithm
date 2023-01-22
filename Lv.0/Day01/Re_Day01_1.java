f799c0f3b32d8c6647cc7c20d10acce7b6cce490
6720 <71584474+2384320@users.noreply.github.com>
2023-01-23 00:38:12 +0900

    Update Re_Day01_1.java

--- Lv.0/Day01/Re_Day01_1.java
+++ Lv.0/Day01/Re_Day01_1.java
@@ -1,7 +1,6 @@
-/* 두 수의 합
-* 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 solution 함수를 완성해주세요. */
 class Solution {
     public int solution(int num1, int num2) {
         return num1 + num2;
     }
 }
+// return으로 바로 값이 빠지도록 별도 변수 생성 없이 실행되도록 함.
