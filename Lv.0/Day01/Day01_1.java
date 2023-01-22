02690095cecf62b967e06d0792cc6a0b546a5177
2384320 <chdms1549@gmail.com>
2023-01-22 23:35:40 +0900

    [Upload] Day01
    Upload Day01 code

--- Lv.0/Day01/Day01_1.java
+++ Lv.0/Day01/Day01_1.java
@@ -1,7 +1,8 @@
+/* 두 수의 합
+* 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 solution 함수를 완성해주세요. */
 class Solution {
     public int solution(int num1, int num2) {
         int answer = num1 + num2;
-        
         return answer;
     }
 }
