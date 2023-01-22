43bce57c9d24fd8fa197482d1d90d0851d25cdde
6720 <71584474+2384320@users.noreply.github.com>
2023-01-23 00:38:28 +0900

    Update Re_Day01_2.java

--- Lv.0/Day01/Re_Day01_2.java
+++ Lv.0/Day01/Re_Day01_2.java
@@ -1,7 +1,6 @@
-/* 두 수의 차
-* 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요. */
 class Solution {
     public int solution(int num1, int num2) {
         return num1 - num2;
     }
 }
+// return으로 바로 값이 빠지도록 별도 변수 생성 없이 실행되도록 함.
