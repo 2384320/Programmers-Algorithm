6ef539930a1bc0bd3eba02131f609599a6a36d98
2384320 <chdms1549@gmail.com>
2023-03-15 02:20:00 +0900

    [Upload] JOIN 03 오랜 기간 보호한 동물 (1)
    Upload JOIN 03 오랜 기간 보호한 동물 (1) code

---
+++ SQL/JOIN/03.sql
@@ -0,0 +1,7 @@
+select t3.name, t3.datetime
+from (
+    select t1.name, t1.datetime
+    from animal_ins t1 left outer join animal_outs t2 on t1.animal_id = t2.animal_id
+    where t2.datetime is null
+    order by t1.datetime) t3
+limit 3;
