ecfeba686b1fd08c9c94b856c953baf3a31a141a
2384320 <chdms1549@gmail.com>
2023-03-15 22:12:43 +0900

    [Upload] JOIN 07 상품 별 오프라인 매출 구하기
    Upload JOIN 07 상품 별 오프라인 매출 구하기 code

---
+++ SQL/JOIN/07.sql
@@ -0,0 +1,4 @@
+select t1.product_code, sum(t1.price * t2.sales_amount) as sales
+from product t1 join offline_sale t2 on t1.product_id = t2.product_id
+group by t1.product_id
+order by sales desc, t1.product_code;
