78356b69f0b51ecf26bc3b7d7f23bbe884247ba0
2384320 <chdms1549@gmail.com>
2023-04-03 16:00:38 +0900

    [Fixed] Lv. 1 03 가운데 글자 가져오기
    Fixed Lv. 1 03 가운데 글자 가져오기 code

--- Lv.1/01~10/03.java
+++ Lv.1/01~10/03.java
@@ -1,16 +1,12 @@
 class Solution {
-    public String solution(int a, int b) {
+    public String solution(String s) {
         String answer = "";
-        int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] dayOfTheWeek = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        int days = 0;
+        String[] arr = s.split("");
+        int size = arr.length;
         
-        for (int i = 1; i < a; i++) {
-            days += months[i-1];
-        }
-
-        days = (days + b - 1) % 7;
-        answer = dayOfTheWeek[days];
+        if (size % 2 == 0) answer = arr[size/2-1] + arr[size/2];
+        else answer = arr[size/2];
+        
         return answer;
     }
 }
