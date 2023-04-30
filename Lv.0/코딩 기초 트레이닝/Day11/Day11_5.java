f50779930fc9302e13a12b3a04682cc977610145
2384320 <chdms1549@gmail.com>
2023-04-30 14:15:29 +0900

    [Upload] Lv. 0 Day11 05 가까운 1 찾기

---
+++ Lv.0/코딩 기초 트레이닝/Day11/Day11_5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        while (idx < arr.length) {
+            if (arr[idx] == 1) {
+                answer = idx;
+                break;
+            }
+            idx++;
+        }
+        return answer;
+    }
+}
