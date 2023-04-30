0ba0503a087fba96a946822b726f4f1451e82e88
2384320 <chdms1549@gmail.com>
2023-04-30 14:16:09 +0900

    [Upload] Lv. 0 Day12 04 2의 영역

---
+++ Lv.0/코딩 기초 트레이닝/Day12/Day12_4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) list.add(i);
+        }
+        
+        if (list.isEmpty()) return new int[] {-1};
+        
+        int[] answer = new int[list.get(list.size()-1) - list.get(0) + 1];
+        int idx = 0;
+        
+        for (int i = list.get(0); i <= list.get(list.size()-1); i++) {
+            answer[idx++] = arr[i];
+        }
+        
+        return answer;
+    }
+}
