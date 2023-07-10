30dc8e9563831d82f54e075f9ce75e488dc80f34
2384320 <chdms1549@gmail.com>
2023-07-10 20:24:27 +0900

    feat: Lv. 2 71 교점에 별 만들기

--- Lv.2/71~80/71.java
+++ Lv.2/71~80/71.java
@@ -1,14 +1,55 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(String s) {
-        int[] answer = new int[2];
-        
-        while (!s.equals("1")) {
-            answer[0]++;
-            answer[1] += s.replaceAll("1", "").length();
-            s = s.replaceAll("0", "");
-            int length = s.length();
-            s = Integer.toString(length, 2);
+    public String[] solution(int[][] line) {        
+        List<long[]> list = new ArrayList<>();
+        for (int i = 0; i < line.length - 1; i++) {
+            for (int j = i + 1; j < line.length; j++) {
+                double a = line[i][0];
+                double b = line[i][1];
+                double e = line[i][2];
+                double c = line[j][0];
+                double d = line[j][1];
+                double f = line[j][2];
+                if (a*d - b*c == 0) continue;
+                double x = (b*f - e*d) / (a*d - b*c);
+                double y = (e*c - a*f) / (a*d - b*c);
+                
+                if ((long) x == x && (long) y == y) list.add(new long[] {(long)x, (long)y});
+            }
         }
+        
+        long[] x = new long[2];
+        long[] y = new long[2];
+        x[0] = Long.MAX_VALUE;
+        x[1] = Long.MIN_VALUE;
+        y[0] = Long.MAX_VALUE;
+        y[1] = Long.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            x[0] = Math.min(x[0], list.get(i)[0]);
+            x[1] = Math.max(x[1], list.get(i)[0]);
+            y[0] = Math.min(y[0], list.get(i)[1]);
+            y[1] = Math.max(y[1], list.get(i)[1]);
+        }
+        
+        long X, Y = 0L;
+        X = x[1] - x[0] + 1;
+        Y = y[1] - y[0] + 1;
+        
+        String[] answer = new String[(int)Y];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = "";
+            for (int j = 0; j < (int) X; j++) {
+                answer[i] += ".";
+            }
+        }
+        
+        for (int i = 0; i < list.size(); i++) {
+            int nx = (int) (list.get(i)[0] - x[0]);
+            int ny = (int) (y[1] - list.get(i)[1]);
+            answer[ny] = answer[ny].substring(0, nx) + "*" + answer[ny].substring(nx + 1);
+        }
+        
         return answer;
     }
 }
