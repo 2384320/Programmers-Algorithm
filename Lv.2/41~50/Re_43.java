027c474cfecbafc735ea2bf282758311e472c24e
2384320 <chdms1549@gmail.com>
2023-07-25 16:26:07 +0900

    refactor: Lv. 2 Re_43 H-Index

---
+++ Lv.2/41~50/Re_43.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int size = citations.length;
+        Arrays.sort(citations);
+        
+        for (int i = size; i > 0; i--) {
+            if (citations[size - i] >= i) return i;
+        }
+        return 0;
+    }
+}
