aafa20a5b426204dade2ebd2d43b43f758b1714c
2384320 <chdms1549@gmail.com>
2023-07-31 15:30:05 +0900

    feat: Lv. 2 87 디펜스 게임

---
+++ Lv.2/81~90/87.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        int length = enemy.length;
+        Queue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < length; i++) {
+            pq.offer(enemy[i]);
+            if (pq.size() > k) n -= pq.poll();
+            if (n < 0) return i;
+        }
+        return length;
+    }
+}
