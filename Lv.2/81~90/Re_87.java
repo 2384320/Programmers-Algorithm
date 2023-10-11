75d813c63e399191b93eb6c8cc0d6878aa00ebb1
2384320 <chdms1549@gmail.com>
2023-10-12 00:06:07 +0900

    refactor: Re_87 디펜스 게임

--- Lv.2/81~90/Re_87.java
+++ Lv.2/81~90/Re_87.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(int n, int k, int[] enemy) {
         int length = enemy.length;
         Queue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < length; i++) {
             pq.offer(enemy[i]);
             if (pq.size() > k)
                 n -= pq.poll();
